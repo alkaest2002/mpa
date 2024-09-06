a3ea1fa708bc337cb6553482acf54f976297c791
Pierpaolo Calanna <p.calanna@gmail.com>
2024-09-06 10:14:34 +0200

    no need to redirect to 404

--- electron-mpa/server.js
+++ electron-mpa/server.js
@@ -6,8 +6,4 @@ const port = 9000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/*", function(req, res, next) {
-    next(res.redirect('/404.html'));
-});
-
 app.listen(port);
