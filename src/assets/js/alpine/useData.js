de05abf1548f6c4621fe23fdadc2d87d37010313
Pierpaolo Calanna <p.calanna@gmail.com>
2024-09-09 10:02:00 +0200

    adding merged reports

--- src/assets/js/alpine/useData.js
+++ src/assets/js/alpine/useData.js
@@ -1,7 +1,25 @@
 import { zipSync, strToU8 } from "fflate";
 
-export default () => ({
+function createIframeFromHtml(htmlString, id, docFragment) {
+  const iframe = document.createElement("iframe");
+  iframe.id = id;
+  iframe.style.height = "100%";
+  iframe.style.width = "100%";
+  iframe.style.border = "none";
+  iframe.src = "data:text/html;charset=utf-8," + encodeURIComponent(htmlString);
+  docFragment.appendChild(iframe);
+}
 
+function mergeHtmlPagesUsingIframes(pages) {
+  const docFragment = document.createDocumentFragment();
+  pages.forEach((page, index) => {
+    createIframeFromHtml(page, `iframe-${index + 1}`, docFragment);
+  });
+  return new XMLSerializer().serializeToString(docFragment);
+}
+
+export default () => ({
+  
   getDataToExport() {
     const testeeData = this.$store.testee.exportState;
     const sessionData = this.$store.session.exportState;
@@ -9,46 +27,60 @@ export default () => ({
     const dataToExport = { ...testeeData, ...sessionData, ...urlsData };
     const condition1 = !this.$store.testee.testeeDataIsSet;
     const condition2 = Object.values(sessionData.data.batteries)
-      .map((battery) => Object.keys(battery.questionnaires)
-        .some(questionnaireId => !Object.keys(sessionData.reports).includes(questionnaireId)))
+      .map((battery) =>
+        Object.keys(battery.questionnaires).some(
+          (questionnaireId) =>
+            !Object.keys(sessionData.reports).includes(questionnaireId)
+        )
+      )
       .some(Boolean);
-    return [condition1, condition2].some(Boolean)
-      ? null
-      : dataToExport;
+    return [condition1, condition2].some(Boolean) ? null : dataToExport;
   },
 
   getExportFileName() {
     if (this.$store.testee.testeeDataIsSet) {
       const { surname, name, yearOfBirth } = this.$store.testee.bio;
-      return `${surname} ${name} ${yearOfBirth}`.toLowerCase().match(/\b\w+\b/g).join("-"); 
+      return `${surname} ${name} ${yearOfBirth}`
+        .toLowerCase()
+        .match(/\b\w+\b/g)
+        .join("-");
     }
     return "anonymous";
   },
 
   downloadJSON(dataToExport, filename) {
-    const dataBlob = new Blob([JSON.stringify(dataToExport)], { type: "application/json" });
+    const dataBlob = new Blob([JSON.stringify(dataToExport)], {
+      type: "application/json",
+    });
     const link = document.createElement("a");
     const url = window.URL.createObjectURL(dataBlob);
     const baseFileName = `${filename}-${Date.now()}`;
     link.href = url;
     link.download = `${baseFileName}.json`;
-    link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
+    link.dataset.downloadurl = ["text/json", link.download, link.href].join(
+      ":"
+    );
     link.click();
     window.URL.revokeObjectURL(url);
   },
 
   downloadZip(dataToExport, filename) {
     const baseFileName = `${filename}-${Date.now()}`;
-    const { reports } = dataToExport;
-    let u8Reports = {}
-    for (const [reportId, report ] of Object.entries(reports)) {
-      u8Reports[`${baseFileName}-report-${reportId}.html`] = strToU8(report);
-    }
     const u8Data = strToU8(JSON.stringify(dataToExport));
-    const zippedData = zipSync({ 
-      data: { [`${baseFileName}-session-data.json`]: u8Data },
-      reports: u8Reports,
-    }, { level: 9 });
+    const { reports: reportsToProcess } = dataToExport;
+    let reports = {};
+    for (const [ reportId, report ] of Object.entries(reportsToProcess)) {
+      reports[`${baseFileName}-report-${reportId}.html`] = strToU8(report);
+    }
+    const mergedReports = strToU8(mergeHtmlPagesUsingIframes(Object.values(reportsToProcess)));
+    const zippedData = zipSync(
+      {
+        data: { [`${baseFileName}-session-data.json`]: u8Data },
+        mergedReports: { [`${baseFileName}-merged-reports.html`]: mergedReports }, 
+        reports,
+      },
+      { level: 9 }
+    );
     const dataBlob = new Blob([zippedData], {
       type: "application/octet-stream",
     });
@@ -59,5 +91,4 @@ export default () => ({
     link.click();
     window.URL.revokeObjectURL(url);
   },
-
-})
+});
