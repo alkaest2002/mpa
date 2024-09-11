347b7a3dfcbf8f46e80c2a3d66c6b48eaed78809
Pierpaolo Calanna <p.calanna@gmail.com>
2024-09-11 20:01:22 +0200

    better variable renaming

--- src/assets/js/worker/scoring.js
+++ src/assets/js/worker/scoring.js
@@ -9,23 +9,23 @@ export default computeScores = ({ testee, session, answers, specs }) => {
   const computeRawScoreStraight = (_, items, answers) => {
     const answerIndices = items.map((el) => el -1);
     const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const straightItemsOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
-    const straightItemsRawScore = answersToKeep
+    const straightOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
+    const straightScore = answersToKeep
       .filter((el) => el.answerValue != "")
       .reduce((acc, itr) => acc + Number(itr), 0);
-    return { straightItemsRawScore, straightItemsOmissions };
+    return { straightScore, straightOmissions };
   }
 
   const computeRawScoreReversed = ({ likert: { max, min }}, items, answers) => {
     const reversedValue = max + min;
     const answerIndices = items.map((el) => el -1);
     const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const reverseItemsdOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
-    const reversedItemsRawScore = answersToKeep
+    const reversedOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
+    const reversedScore = answersToKeep
       .filter((el) => el.answerValue != "")
       .map((el) => reversedValue - el )
       .reduce((acc, itr) => acc + Number(itr), 0);
-    return { reversedItemsRawScore, reverseItemsdOmissions };
+    return { reversedScore, reversedOmissions };
   }
 
   const computeStandardScore = (scaleId, rawScore, specs, testee, session) => {
@@ -36,20 +36,17 @@ export default computeScores = ({ testee, session, answers, specs }) => {
     return Math.round(standardScore, 0);
   }
   
-  const computeScores = (testee, session, answers, specs) => {
-    const answersValues = getAnswersProp(answers, "answerValue");
-    let scores = {};
-    for (const [scaleId, { name, straightItems, reversedItems }] of Object.entries(specs.scales)) {
-      const { straightItemsRawScore, straightItemsOmissions } = computeRawScoreStraight(specs, straightItems, answersValues);
-      const { reversedItemsRawScore, reverseItemsdOmissions } = computeRawScoreReversed(specs, reversedItems, answersValues);
-      const omissions = straightItemsOmissions + reverseItemsdOmissions;
-      const rawScore = straightItemsRawScore + reversedItemsRawScore;
-      const meanScore = rawScore / (straightItems.length + reversedItems.length - omissions);
-      const standardScore = computeStandardScore(scaleId, rawScore, specs, testee, session);
-      scores = { ...scores, [scaleId] : { scaleId, name, rawScore, meanScore, standardScore, omissions }};
-    };
-    return scores;
-  }
-
-  return computeScores(testee, session, answers, specs);
+  let scores = {};
+  const answersValues = getAnswersProp(answers, "answerValue");
+  for (const [scaleId, { name, straightItems, reversedItems }] of Object.entries(specs.scales)) {
+    const { straightScore, straightOmissions } = computeRawScoreStraight(specs, straightItems, answersValues);
+    const { reversedScore, reversedOmissions } = computeRawScoreReversed(specs, reversedItems, answersValues);
+    const omissions = straightOmissions + reversedOmissions;
+    const rawScore = straightScore + reversedScore;
+    const meanScore = rawScore / (straightItems.length + reversedItems.length - omissions);
+    const standardScore = computeStandardScore(scaleId, rawScore, specs, testee, session);
+    scores = { ...scores, [scaleId] : { scaleId, name, rawScore, meanScore, standardScore, omissions }};
+  };
+  
+  return scores;
 };
