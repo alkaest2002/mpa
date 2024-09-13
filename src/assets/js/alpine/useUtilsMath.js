e0e7f502e1536b78a763441396f55ce69a708f78
Pierpaolo Calanna <p.calanna@gmail.com>
2024-09-13 09:47:00 +0200

    moved calculation of delta latency to worker

--- src/assets/js/alpine/useUtilsMath.js
+++
@@ -1,7 +0,0 @@
-export const median = (arr) => {
-  const sorted = arr.slice().sort((a, b) => a - b);
-  const mid = Math.floor(sorted.length / 2);
-  return sorted.length % 2 !== 0
-    ? sorted[mid]
-    : (sorted[mid - 1] + sorted[mid]) / 2;
-};
--- src/assets/js/worker/worker-report.js
+++ src/assets/js/worker/worker-report.js
@@ -1,3 +1,11 @@
+const median = (arr) => {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const mid = Math.floor(sorted.length / 2);
+  return sorted.length % 2 !== 0
+    ? sorted[mid]
+    : (sorted[mid - 1] + sorted[mid]) / 2;
+};
+
 export default onmessage = async ({ data }) => {
   
   importScripts(data.urls.urlScoringScript, data.urls.urlTemplatingScript);
@@ -8,6 +16,14 @@ export default onmessage = async ({ data }) => {
     questionnaire: { questionnaireId, answers }, 
     urls: { urlReportTemplate, urlQuestionnaireSpecs }
   } = data;
+  
+  const itemIds = Object.keys(answers);
+  const answersLatencies = itemIds.map((itemId) => answers[itemId].answerLatency);
+  const medianLatency = median(answersLatencies);
+  itemIds
+    .forEach((itemId) => {
+      answers[itemId]["deltaAnswerLatency"] = answers[itemId].answerLatency - medianLatency;
+    });
 
   const [template, specs] = await Promise.all([
     fetch(urlReportTemplate).then((res) => res.text()), 
