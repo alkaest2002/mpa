776491383be702de78ee3602b7c5db2b9ffe8fa5
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-02 11:54:03 +0200

    bugfixes

--- src/assets/js/worker/scoring.js
+++ src/assets/js/worker/scoring.js
@@ -3,28 +3,32 @@ export default computeScores = ({ testee, session, answers, specs }) => {
   const getAnswersValues = (data) => {
     return Object.values(data)
       .sort((a, b) => a.itemId - b.itemId)
-      .map((el) => el["answerValue"].reduce((acc, itr) => acc += itr, 0));
+      .map((el) => el["answerValue"]);
   }
   
   const computeRawScoreStraight = (_, items, answers) => {
     const answerIndices = items.map((el) => el -1);
+    if (answerIndices.length === 0)
+      return { straightScore: 0, straightOmissions: 0 };
     const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const straightOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
+    const straightOmissions = answersToKeep.filter((el) => el.length === 0)?.length || 0;
     const straightScore = answersToKeep
-      .filter((el) => el.answerValue != "")
-      .reduce((acc, itr) => acc + Number(itr), 0);
+      .filter((el) => el.length > 0)
+      .reduce((acc, itr) => acc += itr.reduce((acc, itr) => acc += Number(itr), 0), 0);
     return { straightScore, straightOmissions };
   }
 
   const computeRawScoreReversed = ({ likert: { max, min }}, items, answers) => {
-    const reversedValue = max + min;
     const answerIndices = items.map((el) => el -1);
+    if (answerIndices.length === 0)
+      return { reversedScore: 0, reversedOmissions: 0 };
+    const reversedValue = max + min;
     const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const reversedOmissions = answersToKeep.filter((el) => el.answerValue == "")?.length || 0;
+    const reversedOmissions = answersToKeep.filter((el) => el.length === 0)?.length || 0;
     const reversedScore = answersToKeep
-      .filter((el) => el.answerValue != "")
-      .map((el) => reversedValue - el )
-      .reduce((acc, itr) => acc + Number(itr), 0);
+      .filter((el) => el.length > 0)
+      .map((el) => reversedValue - el.reduce((acc, itr) => acc += Number(itr), 0))
+      .reduce((acc, itr) => acc += itr);
     return { reversedScore, reversedOmissions };
   }
 
@@ -37,7 +41,7 @@ export default computeScores = ({ testee, session, answers, specs }) => {
   }
   
   let scores = {};
-  const answersValues = getAnswersValues(answers)
+  const answersValues = getAnswersValues(answers);
   for (const [scaleId, { name, straightItems, reversedItems }] of Object.entries(specs.scales)) {
     const { straightScore, straightOmissions } = computeRawScoreStraight(specs, straightItems, answersValues);
     const { reversedScore, reversedOmissions } = computeRawScoreReversed(specs, reversedItems, answersValues);
