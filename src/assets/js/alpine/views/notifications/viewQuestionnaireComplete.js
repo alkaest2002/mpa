6984e4873e19daffcfcfabde9f9613753392d51b
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-02 07:08:44 +0200

    removed unused code

--- src/assets/js/alpine/views/notifications/viewQuestionnaireComplete.js
+++ src/assets/js/alpine/views/notifications/viewQuestionnaireComplete.js
@@ -1,4 +1,3 @@
-import css from "../../cssClasses.json";
 import useNavigation from "../../use/useNavigation";
 
 const { goToCurrentBattery } = useNavigation();
@@ -14,12 +13,7 @@ export default () => ({
       ["@click.prevent"]() {
         this.$store.session.completedQuestionnaires.includes(this.$store.session.questionnaireId) 
           && goToCurrentBattery.call(this);
-      },
-      [":class"]() {
-        return this.$store.session.completedQuestionnaires.includes(this.$store.session.questionnaireId)
-          ? css.selected
-          : css.nonSelected;
-      },
+      }
     }
   }
 })
