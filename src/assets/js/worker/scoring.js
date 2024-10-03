f1d566cc15c7bb329d10cf2a5a6c6d53ae4e1881
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-03 13:29:20 +0200

    updated scoring js

--- src/assets/js/worker/scoring.js
+++ src/assets/js/worker/scoring.js
@@ -1,57 +1,78 @@
 export default computeScores = ({ testee, session, answers, specs }) => {
 
-  const getAnswersValues = (data) => {
+  const getAnswers = (data) => {
     return Object.values(data)
       .sort((a, b) => a.itemId - b.itemId)
       .map((el) => el["answerValue"]);
   }
+
+  const getScaleAnswers = (items, answers) => {
+    const answerIndices = items.map((el) => el -1);
+    const scaleAllAnswers = answers.filter((_, index) => answerIndices.includes(index))
+    return {
+      scaleAnswers: scaleAllAnswers.filter((el) => el.length > 0),
+      scaleOmissions: scaleAllAnswers.filter((el) => el.length === 0) || [],
+    };
+  }
   
   const computeRawScoreStraight = (_, items, answers) => {
     // answers is a list of lists: example [[1],[2,4],[1],[5]]
-    const answerIndices = items.map((el) => el -1);
-    if (answerIndices.length === 0)
-      return { straightScore: 0, straightOmissions: 0 };
-    const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const straightOmissions = answersToKeep.filter((el) => el.length === 0)?.length || 0;
-    const straightScore = answersToKeep
-      .filter((el) => el.length > 0)
-      .reduce((acc, itr) => acc += itr.reduce((acc, itr) => acc += Number(itr), 0), 0);
-    return { straightScore, straightOmissions };
+    if (items.length === 0)
+      return { straightScore: null, straightOmissions: null, straightMin: null, straightMax: null };
+    const { scaleAnswers, scaleOmissions } = getScaleAnswers(items, answers);
+    const flattenedScaleAnswers = scaleAnswers.flat(Infinity);
+    const straightScore = flattenedScaleAnswers.reduce((acc, itr) => acc += Number(itr), 0);
+    const { 0: minValue, [scaleAnswers.length - 1]: maxValue } = flattenedScaleAnswers.sort();
+    return { 
+      straightScore, 
+      straightOmissions: scaleOmissions.length, 
+      straightMin: minValue, 
+      straightMax: maxValue
+    };
   }
 
   const computeRawScoreReversed = ({ likert: { max, min }}, items, answers) => {
     // answers is a list of lists: example [[1],[2,4],[1],[5]]
-    const answerIndices = items.map((el) => el -1);
-    if (answerIndices.length === 0)
-      return { reversedScore: 0, reversedOmissions: 0 };
-    const reversedValue = max + min;
-    const answersToKeep = answers.filter((_, index) => answerIndices.includes(index));
-    const reversedOmissions = answersToKeep.filter((el) => el.length === 0)?.length || 0;
-    const reversedScore = answersToKeep
-      .filter((el) => el.length > 0)
-      .map((el) => reversedValue - el.reduce((acc, itr) => acc += Number(itr), 0))
-      .reduce((acc, itr) => acc += itr);
-    return { reversedScore, reversedOmissions };
+    if (items.length === 0)
+      return { reversedScore: null, reversedOmissions: null, reversedMin: null, reversedMax: null };
+    const { scaleAnswers, scaleOmissions } = getScaleAnswers(items, answers);
+    const flattenedScaleAnswers = scaleAnswers.flat(Infinity);
+    const reversedValue = Number(max + min);
+    const reversedScore = flattenedScaleAnswers.reduce((acc, itr) => acc += reversedValue - Number(itr));
+    const { 0: minValue, [scaleAnswers.length - 1]: maxValue } = flattenedScaleAnswers.sort();
+    return { 
+      reversedScore, 
+      reversedOmissions: scaleOmissions.length, 
+      reversedMin: reversedValue - maxValue, 
+      reversedMax: reversedValue - minValue
+    };
   }
 
-  const computeStandardScore = (scaleId, rawScore, meanScore, specs, testee, session) => {
+  const computeStandardScore = (scaleId, scores, specs, testee, session) => {
     const getNormsFn = specs.norms.getNorms;
     const currentNormsId = eval?.(`"use strict";${getNormsFn};fn(${JSON.stringify({ ...testee, ...session })},"${scaleId}")`);
     const currentNorms = specs.norms[currentNormsId];
-    const standardScore =  eval?.(`"use strict";${currentNorms};fn(${JSON.stringify({ rawScore, meanScore })})`);
+    const standardScore =  eval?.(`"use strict";${currentNorms};fn(${JSON.stringify(scores)})`);
     return standardScore;
   }
   
-  let scores = {};
-  const answersValues = getAnswersValues(answers);
+  // init var
+  let allScores = {};
+  // get answer values
+  const answersValues = getAnswers(answers);
+  // compute scores for each scale
   for (const [scaleId, { name, straightItems, reversedItems }] of Object.entries(specs.scales)) {
-    const { straightScore, straightOmissions } = computeRawScoreStraight(specs, straightItems, answersValues);
-    const { reversedScore, reversedOmissions } = computeRawScoreReversed(specs, reversedItems, answersValues);
-    const omissions = straightOmissions + reversedOmissions;
-    const rawScore = straightScore + reversedScore;
+    const { straightScore, straightOmissions, straightMin, straightMax } = computeRawScoreStraight(specs, straightItems, answersValues);
+    const { reversedScore, reversedOmissions, reversedMin, reversedMax } = computeRawScoreReversed(specs, reversedItems, answersValues);
+    const omissions = (straightOmissions || 0) + (reversedOmissions || 0);
+    const rawScore = (straightScore || 0) + (reversedScore || 0);
     const meanScore = Number((rawScore / (straightItems.length + reversedItems.length - omissions)).toFixed(2));
-    const standardScore = computeStandardScore(scaleId, rawScore, meanScore, specs, testee, session);
-    scores = { ...scores, [scaleId] : { scaleId, name, rawScore, meanScore, standardScore, omissions }};
+    const minScore = [ straightMin, reversedMin ].filter((el) => el || el === 0).sort().at(0);
+    const maxScore = [ straightMax, reversedMax ].filter((el) => el || el === 0).sort().at(-1);
+    const scores = { omissions, rawScore, meanScore, minScore, maxScore };
+    const standardScore = computeStandardScore(scaleId, scores, specs, testee, session);
+    allScores = { ...allScores, [scaleId] : { scaleId, name, ...scores, standardScore }};
   };
-  return scores;
+  // return all scales scores
+  return allScores;
 };
