292d92637a666dc3b706cf90f31cee231585e7ad
Pierpaolo Calanna <p.calanna@gmail.com>
2024-10-04 08:37:10 +0200

    refactored scoring

--- src/assets/js/worker/scoring.js
+++ src/assets/js/worker/scoring.js
@@ -6,45 +6,28 @@ export default computeScores = ({ testee, session, answers, specs }) => {
       .map((el) => el["answerValue"]);
   }
 
-  const getScaleAnswers = (items, answers) => {
+  const getComponentAnswers = (items, answers) => {
     const answerIndices = items.map((el) => el -1);
     const scaleAllAnswers = answers.filter((_, index) => answerIndices.includes(index))
     return {
-      scaleAnswers: scaleAllAnswers.filter((el) => el.length > 0),
-      scaleOmissions: scaleAllAnswers.filter((el) => el.length === 0) || [],
-    };
-  }
-  
-  const computeRawScoreStraight = (_, items, answers) => {
-    // answers is a list of lists: example [[1],[2,4],[1],[5]]
-    if (items.length === 0)
-      return { straightScore: null, straightOmissions: null, straightMin: null, straightMax: null };
-    const { scaleAnswers, scaleOmissions } = getScaleAnswers(items, answers);
-    const flattenedScaleAnswers = scaleAnswers.flat(Infinity);
-    const straightScore = flattenedScaleAnswers.reduce((acc, itr) => acc += Number(itr), 0);
-    const { 0: minValue, [scaleAnswers.length - 1]: maxValue } = flattenedScaleAnswers.sort();
-    return { 
-      straightScore, 
-      straightOmissions: scaleOmissions.length, 
-      straightMin: minValue, 
-      straightMax: maxValue
+      componentAnswers: scaleAllAnswers.filter((el) => el.length > 0),
+      componentOmissions: scaleAllAnswers.filter((el) => el.length === 0) || [],
     };
   }
 
-  const computeRawScoreReversed = ({ likert: { max, min }}, items, answers) => {
+  const computeRawScoreComponent = (items, answers, cb = (el) => el) => {
     // answers is a list of lists: example [[1],[2,4],[1],[5]]
     if (items.length === 0)
-      return { reversedScore: null, reversedOmissions: null, reversedMin: null, reversedMax: null };
-    const { scaleAnswers, scaleOmissions } = getScaleAnswers(items, answers);
-    const flattenedScaleAnswers = scaleAnswers.flat(Infinity);
-    const reversedValue = Number(max + min);
-    const reversedScore = flattenedScaleAnswers.reduce((acc, itr) => acc += reversedValue - Number(itr));
-    const { 0: minValue, [scaleAnswers.length - 1]: maxValue } = flattenedScaleAnswers.sort();
+      return { componentScore: null, componentOmissions: null, componentMin: null, componentMax: null };
+    const { componentAnswers, componentOmissions } = getComponentAnswers(items, answers);
+    const componentAnswersFlat = componentAnswers.flat(Infinity).map(el => cb(Number(el)));
+    const componentRawScore = componentAnswersFlat.reduce((acc, itr) => acc += itr, 0);
+    const { 0: minValue, [componentAnswers.length - 1]: maxValue } = componentAnswersFlat.sort();
     return { 
-      reversedScore, 
-      reversedOmissions: scaleOmissions.length, 
-      reversedMin: reversedValue - maxValue, 
-      reversedMax: reversedValue - minValue
+      componentRawScore, 
+      componentOmissions: componentOmissions.length, 
+      componentMin: minValue, 
+      componentMax: maxValue
     };
   }
 
@@ -56,14 +39,28 @@ export default computeScores = ({ testee, session, answers, specs }) => {
     return standardScore;
   }
   
-  // init var
-  let allScores = {};
+  // init scale scores obj
+  let scaleScores = {};
   // get answer values
   const answersValues = getAnswers(answers);
   // compute scores for each scale
   for (const [scaleId, { name, straightItems, reversedItems }] of Object.entries(specs.scales)) {
-    const { straightScore, straightOmissions, straightMin, straightMax } = computeRawScoreStraight(specs, straightItems, answersValues);
-    const { reversedScore, reversedOmissions, reversedMin, reversedMax } = computeRawScoreReversed(specs, reversedItems, answersValues);
+    // compute straight items component
+    const { 
+      componentRawScore: straightScore, 
+      componentOmissions: straightOmissions, 
+      componentMin: straightMin, 
+      componentMax: straightMax 
+    } = computeRawScoreComponent(straightItems, answersValues);
+    // compute reversed items component
+    const cb = (el) => specs.likert.max + specs.likert.min - el;
+    const { 
+      componentRawScore: reversedScore, 
+      componentOmissions: reversedOmissions, 
+      componentMin: reversedMin, 
+      componentMax: reversedMax 
+    } = computeRawScoreComponent(reversedItems, answersValues, cb);
+    // merge components together to get scale scores
     const omissions = (straightOmissions || 0) + (reversedOmissions || 0);
     const rawScore = (straightScore || 0) + (reversedScore || 0);
     const meanScore = Number((rawScore / (straightItems.length + reversedItems.length - omissions)).toFixed(2));
@@ -71,8 +68,8 @@ export default computeScores = ({ testee, session, answers, specs }) => {
     const maxScore = [ straightMax, reversedMax ].filter((el) => el || el === 0).sort().at(-1);
     const scores = { omissions, rawScore, meanScore, minScore, maxScore };
     const standardScore = computeStandardScore(scaleId, scores, specs, testee, session);
-    allScores = { ...allScores, [scaleId] : { scaleId, name, ...scores, standardScore }};
+    scaleScores = { ...scaleScores, [scaleId] : { scaleId, name, ...scores, standardScore }};
   };
   // return all scales scores
-  return allScores;
+  return scaleScores;
 };
